f4043e8dfbbfffc57c5e17bfdfae6d18d325476e
takumus <me@takum.us>
2020-03-28 21:03:37 +0900

    utilsへ分離。そしてスコープ付きぱっけいーじ名に対応

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
 #!/usr/bin/env node
-const rl = require('readline-sync');
 const fs = require('fs');
 const path = require('path');
 const generators = require('./generators');
+const utils = require('./utils');
 
 const projectRootDir = process.cwd();
+const libraryTemplateDir = './node_modules/@takumus/typescript-library-template'
 
 function init(options) {
   console.log(`create library at : ${projectRootDir}`);
   // generate projectName from dir path
   options.projectName = path.basename(projectRootDir);
 }
+
 function input(options) {
-  options.projectName = readlineStr('project name', options.projectName).toLowerCase();
-  options.destDir = readlineStr('destination dir', options.destDir);
+  options.projectName = utils.readlineStr('project name', options.projectName).toLowerCase();
+  options.destDir = utils.readlineStr('destination dir', options.destDir);
   // generate default browserGlobalName from projectName
-  options.browserGlobalName = readlineStr('global name for browser', toCamelCase(options.projectName));
-  options.entryFile = readlineStr('entry file', options.entryFile);
-  options.author.name = readlineStr('author.name', options.author.name);
-  options.author.email = readlineStr('author.email', options.author.name);
+  options.browserGlobalName = utils.readlineStr('global name for browser', utils.toBrowserName(options.projectName));
+  options.entryFile = utils.readlineStr('entry file', options.entryFile);
+  options.author.name = utils.readlineStr('author.name', options.author.name);
+  options.author.email = utils.readlineStr('author.email', options.author.name);
 }
+
 function beforeGenerate(options) {
   // create entryFileName from entryFile
   options.entryFileName = path.basename(options.entryFile, path.extname(options.entryFile));
   // create entryDir from entryFile
   options.entryDir = path.dirname(options.entryFile);
 }
+
 function generateAll(options) {
   // generate files
   generate("package.json", generators.packageJSON, options);
@@ -45,12 +49,13 @@ function generateAll(options) {
   const tsFile = path.resolve(projectRootDir, options.entryFile);
   if (!fs.existsSync(tsFile)) fs.writeFileSync(tsFile, '', {});
 }
+
 function afterGenerate(options) {
   console.log('complete!\nyou should run `npm install` and `npm run build`');
 }
 
 function generate(name, generator, options) {
-  const file = path.resolve(__dirname, './node_modules/@takumus/typescript-library-template', name);
+  const file = path.resolve(__dirname, libraryTemplateDir, name);
   let body = '';
   try {
     body = fs.readFileSync(file).toString();
@@ -63,17 +68,8 @@ function generate(name, generator, options) {
     )
   );
 }
-function readlineStr(message, defaultInput) {
-  const value = rl.question(`${message}(${defaultInput}):`, {
-    defaultInput: defaultInput
-  });
-  return value === '' ? defaultInput : value;
-}
-function toCamelCase(value) {
-  return value.split("-").map((v) => v.charAt(0).toUpperCase() + v.substr(1)).join('');
-}
-
 (() => {
+  // default values
   const options = {
     projectName: '',
     browserGlobalName: '',
@@ -86,6 +82,7 @@ function toCamelCase(value) {
       email: ''
     }
   };
+  // do tasks
   init(options);
   input(options);
   beforeGenerate(options);
